731de9bdfd7f07c5fdd71a707792308d8bce79e1
Veronica Giro <veronica.giro@quantide.com>
2016-09-19 12:54:39 +0200

    qtruncate: lower.tail e log.p sistemato per d e p, forse per q

--- qtruncate/R/dtruncate.R
+++ qtruncate/R/dtruncate.R
@@ -1,60 +1,66 @@
 dtruncate <-
-  function (dist){ 
-    
-    ddist=paste("d", dist, sep = "") 
+  function (dist){
+
+    ddist=paste("d", dist, sep = "")
     pdist=paste("p", dist, sep = "")
 
-    #gets density function                    
+    #gets density function
     ddist <-  get(ddist, mode = "function")
     #gets argument of density function
     dargs <- formals(ddist)
-   
 
-    #gets probability function                    
+
+    #gets probability function
     pdist <- get(pdist, mode = "function")
     #gets argument of probability function
     pargs <- formals(pdist)
-    
+
     #Output function starts here
-    density <- function () 
+    density <- function ()
     {
-      #check L U 
+      #check L U
       if (L > U) stop("U must be greater than or equal to L")
-      
+
       #gets density arguments
-      
+
       call <- as.list(match.call())[-1]
-      
+
       dargs <- intersect_args(x = dargs, y = call)
-      
-      #all unique arguments belonging to probability and pdist 
+      dargs$log <- NULL
+
+      #all unique arguments belonging to probability and pdist
       pargs <- intersect_args(x = pargs, y = call)
-      
+      pargs$log <- NULL
+
       #select x only where defined by L and U
       dargs$x <- x[x > L & x <= U]
-      
+
       #define arguments for pdist in L and U
-      pUargs <-  pLargs <- pargs 
+      pUargs <-  pLargs <- pargs
       pUargs$q <- U
       pLargs$q <- L
-      
+
       #initialize output
       density <- numeric(length(x))
-      
+
       #this is standard method for computing density values for truncated distributions
-      
+
       dx <- do.call("ddist", as.list(dargs))
       pU <- do.call("pdist", as.list(pUargs))
-      pL <- do.call("pdist", as.list(pLargs))               
-      
+      pL <- do.call("pdist", as.list(pLargs))
 
       density[x > L & x <= U] <-  dx /(pU - pL)
-      
+
       #returns density values for truncated distributions
+
+      if(log){
+        density <- log(density)
+      }
+
       return(density)
-      
+
     }
-    
+
     #add to density function formals L and U with values as passed with dtruncate
     formals(density) <-  c(formals(ddist), eval(substitute(alist(L=-Inf, U=Inf))))
     #return density function
--- qtruncate/R/qtruncate.R
+++ qtruncate/R/qtruncate.R
@@ -1,49 +1,64 @@
 qtruncate <-
   function (dist){
-      
+
     #dist <- deparse(substitute(dist))
-    
-    qdist=paste("q", dist, sep = "") 
+
+    qdist=paste("q", dist, sep = "")
     pdist=paste("p", dist, sep = "")
-    
+
     qdist <-  get(qdist, mode = "function")
     qargs <-  formals(qdist)
-    
+
     pdist <-  get(pdist, mode = "function")
     pargs <- formals(pdist)
-    
-    quantile <- function () 
+
+    quantile <- function ()
     {
       if (L > U) stop("U must be greater than or equal to L")
-      
+
       call <- as.list(match.call())[-1]
       #qargs <- c(qargs[!is.element(names(qargs), names(call))], call[is.element(names(call), names(qargs))])
       qargs <- intersect_args(x = qargs, y = call)
-      
-      
+      qargs$log.p <- NULL
+      qargs$lower.tail <- NULL
+
       pargs <- c(pargs[!is.element(names(pargs), names(call))], call[is.element(names(call), names(pargs))])
       pargs <- intersect_args(x = pargs, y = call)
-      
-      pUargs <- pLargs <- pargs 
+      pargs$log.p <- NULL
+      pargs$lower.tail <- NULL
+
+      pUargs <- pLargs <- pargs
       pUargs$q <- U
       pLargs$q <- L
-      
-      if ( do.call("pdist", as.list(pUargs)) == 0) stop("U below lower support limit") 
+
+      # non funziona con log.p =TRUE (per il momento commento)
+      if ( do.call("pdist", as.list(pUargs)) == 0) stop("U below lower support limit")
       if ( do.call("pdist", as.list(pLargs)) == 1) stop("L above upper support limit")
-      
-      qargs$p <- do.call("pdist", as.list(pLargs)) + p * (do.call("pdist", as.list(pUargs)) - do.call("pdist", as.list(pLargs))) 
-      
+
+      qargs$p <- do.call("pdist", as.list(pLargs)) + p * (do.call("pdist", as.list(pUargs)) - do.call("pdist", as.list(pLargs)))
+
+
+      if(lower.tail == F){
+        qargs$p <- 1 - qargs$p
+      }
+
+
+      if(log.p){
+       qargs$p <- log(qargs$p)
+      }
+
       qp <- do.call("qdist", as.list(qargs))
       #quantile <- pmin(pmax(L,do.call("qdist", as.list(qargs))),U)
+
       quantile <- pmin(pmax(L, qp ), U)
-      
+
       return(quantile)
-      
+
     }
-    
+
     formals(quantile) <-  c(formals(qdist), eval(substitute(alist(L=-Inf, U=Inf))))
-    
+
     return(quantile)
-    
+
   }
 
